26aaf1ead039dc3db5c2754d3d7fc315eeb292e2
unpred1ctable <88487354+unpred1ctable@users.noreply.github.com>
2023-01-17 09:40:02 +0300

    changes

--- 1.1.4-1.1.4/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ 1.1.4-1.1.4/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    Connection connection = Util.getConnection();
+    private Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
     }
